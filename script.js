d97c24184c44fdb6c98241fa398408ba1ef1aea4
Joseph Broz <brozj11@gmail.com>
2020-08-12 19:44:46 -0700

    finish exercise

--- script.js
+++ script.js
@@ -19,43 +19,70 @@ document.addEventListener('DOMContentLoaded', function(event) {
   // Do all of your work inside the document.addEventListener
 
   // Part 1
-  function shortenPageTitle() {
-
-  }
+  (function shortenPageTitle() {
+    document.querySelector('#main-title').innerHTML = 'DOM';
+    // main_title
+  })();
 
 
   // Part 2
-  function changeBodyBgColor() {
-
-  }
+  (function changeBodyBgColor() {
+    document.querySelector('body').style.backgroundColor = 'pink';
+  })();
 
 
   // Part 3
-  function removeLastFavoriteThing() {
-
-  }
+  (function removeLastFavoriteThing() {
+    document.querySelector('#favorite-things').lastChild.remove();
+    document.querySelector('#favorite-things').lastChild.remove();
+  })();
 
 
   // Part 4
-  function makeSpecialTitlesBigger() {
-
-  }
+  (function makeSpecialTitlesBigger() {
+    for (var element of document.querySelectorAll('.special-title')) {
+      element.style.fontSize = '2em'
+    }
+  })();
 
 
   // Part 5
-  function RemoveChicagoRace() {
-
-  }
+  (function RemoveChicagoRace() {
+    for (var child of document.querySelector('#past-races').children) {
+      (child.innerText.toLowerCase() == 'chicago') && child.remove();
+    }
+  })();
 
   // Part 6
-  function addPastRace() {
+  (function addPastRace() {
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Chicago is actually the best city");
+    node.appendChild(textnode);
+    document.querySelector('#past-races').appendChild(node);
+  })();
 
-  }
+
+  // Create a new `.blog-post` corresponding to the new city added in **Part 6**. 
+  // You will have to create a new `<div>` with class of `.blog-post`, a new `<h2>` 
+  // with text, and a new `<p>` with some text. Think about what order you want to 
+  // create the elements, and what order you want to append them in.
 
 
   // Part 7
-  function createNewBlogPost() {
-
-  }
+  (function createNewBlogPost() {
+    var topnode = document.createElement("div");
+    topnode.classList.add('blog-post');
+    topnode.classList.add('purple');
+    var headernode = document.createElement('h2');
+    var h_textnode = document.createTextNode('Chicago');
+    headernode.appendChild(h_textnode);
+    var pnode = document.createElement('p');
+    var p_textnode = document.createTextNode('Some stuff about how Chicago is the best city');
+    pnode.appendChild(p_textnode);
+    var refnode = document.querySelector('.main').lastChild;
+    topnode.appendChild(headernode);
+    topnode.appendChild(pnode);
+    refnode.after(topnode);
+  })();
 
 });
